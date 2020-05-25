87ddaaf16b577f75843c36e25ed10018ef7c3786
MrBartou <53816747+MrBartou@users.noreply.github.com>
2020-05-25 11:45:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+import pyaudio
+import wave
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile as wf
 from scipy import fft
 
-sons =["song_library/song1.wav", "song_library/song2.wav", "song_library/song3.wav"]
+sons =["test.wav"]
+
+def record():
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 10
+    WAVE_OUTPUT_FILENAME = "test.wav"
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+    print ("Enregistrement en cours...")
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print ("Fin d'enregistrement")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
 
 def traitement(son):
     sampFreq, snd = wf.read(son)
@@ -41,7 +70,7 @@ def traitement(son):
     return song_value
 
 for i in range(len(sons)):
-    print(sons[i])
+    record()
     song_value = traitement(sons[i])
     if song_value == 0.2076:
         print("pompier")
