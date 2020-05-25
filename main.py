971e588d57396d89840a848268e0504dd99d7314
BaresTheo <barestheo@localhost.localdomain>
2020-05-25 21:52:40 +0200

    [ADD] graphics -> graphics window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyaudio
+from tkinter import *
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
@@ -60,7 +61,7 @@ def traitement(son):
         p[1:len(p)] = p[1:len(p)] * 2
     else:
         p[1:len(p) -1] = p[1:len(p) - 1] * 2
-    freqArray = np.arange(0, nUniquePts, 1.0) * (sampFreq / n);
+    freqArray = np.arange(0, nUniquePts, 1.0) * (sampFreq / n)
     plt.plot(freqArray/50, 10*np.log10(p), color='r')
     plt.xlabel('Frequence (kHz)')
     plt.ylabel('Puissance (dB)')
@@ -69,12 +70,55 @@ def traitement(son):
     song_value = round(song_value,4)
     return song_value
 
+def drawcircle(canv, x, y, rad, back):
+    canv.create_oval(x - rad, y - rad, x + rad, y + rad, width = 0, fill = back)
+
+def interpret_message(message):
+    window = Tk()
+    window.title("ACPM")
+    window.geometry("800x600")
+    position = 4
+    if (message == "urgent"):
+        back = 'red'
+    elif (message == "klaxon"):
+        back = 'yellow'
+    elif (message == "ambiant"):
+        back = 'green'
+
+    if (position == 0):
+        x = 50
+        y = 300
+    elif (position == 1):
+        x = 133
+        y = 300
+    elif (position == 2):
+        x = 275
+        y = 300
+    elif (position == 3):
+        x = 500
+        y = 300
+    elif (position == 4):
+        x = 644
+        y = 300
+    elif (position == 5):
+        x = 735
+        y = 300
+    canvas = Canvas(window, width = 800, height = 600, bg = 'white')
+    photo = PhotoImage(file = 'voiture.png')
+    canvas.create_image(400, 300, image = photo)
+    canvas.pack()
+    drawcircle(canvas, x, y, 20, back)
+    window.mainloop()
+
 for i in range(len(sons)):
     record()
     song_value = traitement(sons[i])
     if song_value == 0.2076:
-        print("pompier")
+        message = "urgent"
+        interpret_message(message)
     if song_value == 0.1925:
-        print("police")
+        message = "urgent"
+        interpret_message(message)
     if song_value == 0.2394:
-        print("klaxon")
+        message = "klaxon"
+        interpret_message(message)
